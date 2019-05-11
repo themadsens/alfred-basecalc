211e31bee903035a7f55828ac6a84e7b465a3bb3
Flemming Madsen <flemming@themadsens.dk>
2019-05-11 22:23:21 +0200

    Initial upload

---
+++ pcalc.lua
@@ -0,0 +1,68 @@
+#!/usr/local/bin/lua-5.3
+
+local json = require 'json' .encode
+
+local res, err = load("return "..arg[1], "expr", "t", math)
+if not res then
+   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = err:sub(20)}}})
+end
+
+local res, val = pcall(res)
+if not res then
+   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = val }}})
+end
+if type(val) ~= 'number' then
+   return print(json{items={{icon = {path = "err.png"}, title = "Not a number: "..type(val), subtitle = tostring(val)}}})
+end
+if math.type(val) == 'float' then
+   local str
+   if math.abs(val) < 1e-6 or math.abs(val) > 1e14 then
+      str = ("%.16g"):format(val)
+   else
+      str = ("%.16f"):format(val)
+      str = str:match('(%d*%.0?%d-)0*$')
+   end
+   return print(json{items={{icon = {path = "10.png"}, title = str, arg = str}}})
+end
+
+function reverse(arr)
+   local i, j = 1, #arr
+
+   while i < j do
+      arr[i], arr[j] = arr[j], arr[i]
+      i = i + 1
+      j = j - 1
+   end
+   return arr
+end
+
+function bits(num)
+  local t={}
+  while num ~= 0 do
+    bit = num & 1
+    table.insert(t, bit)
+    num = num >> 1
+  end
+  return table.concat(reverse(t))
+end
+
+function group(str, len, sep)
+  local t={}
+  while #str > 0 do
+    table.insert(t, str:sub(-len))
+    str = str:sub(1, -len-1)
+  end
+  return table.concat(reverse(t), sep)
+end
+
+local th = utf8.char(0x2009)
+local str10 = ("%d"):format(val)
+local str16 = ("%x"):format(val)
+local str8  = ("%o"):format(val)
+local str2  = bits(val)
+print(json {items={
+   {icon = {path = "10.png"}, title=group(str10, 3, '.'), arg=str10},
+   {icon = {path = "16.png"}, title=group(str16, 4,  th), arg="0x"..str16},
+   {icon = {path =  "8.png"}, title=      str8,           arg="0o"..str8},
+   {icon = {path =  "2.png"}, title=group(str2,  4,  th), arg="0b"..str2},
+}})
