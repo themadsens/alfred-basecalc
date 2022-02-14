c0c9d75b02c88285a3be8ecb44369ee6c82f2c59
Flemming Madsen <fm@amplex.dk>
2022-02-14 08:14:21 +0100

    Group integer part of float

--- pcalc.lua
+++ pcalc.lua
@@ -5,31 +5,14 @@ local tofrac = require 'fraction'
 math.e = math.exp(1)
 math.log10 = function(v) return math.log(v, 10) end
 
-local res, err = load("return "..arg[1], "expr", "t", math)
-if not res then
-   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = err:sub(20)}}})
-end
-
-local res, val = pcall(res)
-if not res then
-   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = val }}})
-end
-if type(val) ~= 'number' then
-   return print(json{items={{icon = {path = "err.png"}, title = "Not a number: "..type(val), subtitle = tostring(val)}}})
-end
-if math.type(val) == 'float' then
-   local str
-   if math.abs(val) < 1e-6 or math.abs(val) > 1e14 then
-      str = ("%.16g"):format(val)
-   else
-      str = ("%.16f"):format(val)
-      str = str:match('(%d*%.0?%d-)0*$')
-   end
-   local nom, den, err = tofrac(val)
-   return print(json{items={
-      {icon = {path = "10.png"}, title = str, arg = str},
-      {icon = {path = "fr.png"}, title = nom.." / "..den, arg = nom.."/"..den}
-   }})
+local th = utf8.char(0x2009)
+function group(str, len, sep)
+  local t={}
+  while #str > 0 do
+    table.insert(t, str:sub(-len))
+    str = str:sub(1, -len-1)
+  end
+  return table.concat(reverse(t), sep)
 end
 
 function reverse(arr)
@@ -53,16 +36,35 @@ function bits(num)
   return table.concat(reverse(t))
 end
 
-function group(str, len, sep)
-  local t={}
-  while #str > 0 do
-    table.insert(t, str:sub(-len))
-    str = str:sub(1, -len-1)
-  end
-  return table.concat(reverse(t), sep)
+local res, err = load("return "..arg[1], "expr", "t", math)
+if not res then
+   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = err:sub(20)}}})
 end
 
-local th = utf8.char(0x2009)
+local res, val = pcall(res)
+if not res then
+   return print(json{items={{icon = {path = "err.png"}, title = "Error", subtitle = val }}})
+end
+if type(val) ~= 'number' then
+   return print(json{items={{icon = {path = "err.png"}, title = "Not a number: "..type(val), subtitle = tostring(val)}}})
+end
+if math.type(val) == 'float' then
+   local str
+   if math.abs(val) < 1e-6 or math.abs(val) > 1e19 then
+      str = ("%.16g"):format(val)
+   else
+      str = ("%.16f"):format(val)
+      local int, dec = str:match('(%d*)%.(0?%d-)0*$')
+      dec = dec:gsub('00000000000+%d?%d$', '')
+      str = group(int, 3, th).."."..dec
+   end
+   local nom, den, err = tofrac(val)
+   return print(json{items={
+      {icon = {path = "10.png"}, title = str, arg = str},
+      {icon = {path = "fr.png"}, title = nom.." / "..den, arg = nom.."/"..den}
+   }})
+end
+
 local str10 = ("%d"):format(val)
 local str16 = ("%x"):format(val)
 local str8  = ("%o"):format(val)
